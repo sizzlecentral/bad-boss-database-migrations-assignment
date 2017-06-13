92bb6c777ba5f248b30ced5c0ab947d290414832
Don Cooper <don_cooper@hotmail.com>
2017-06-13 16:55:20 -0400

    Locations table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613204803) do
+ActiveRecord::Schema.define(version: 20170613205439) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "province_state"
+    t.string "country"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
