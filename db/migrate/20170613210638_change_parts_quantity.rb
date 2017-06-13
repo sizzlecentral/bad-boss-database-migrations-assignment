a62b52e2ab8a26daba15210b21028e4608b3d23d
Don Cooper <don_cooper@hotmail.com>
2017-06-13 17:11:57 -0400

    Changed type in Parts to decimal from integer

---
+++ db/migrate/20170613210638_change_parts_quantity.rb
@@ -0,0 +1,11 @@
+class ChangePartsQuantity < ActiveRecord::Migration[5.0]
+
+  def up
+    change_column :parts, :quantity, :decimal
+  end
+
+  def down
+    change_column :parts, :quantity, :integer
+  end
+
+end
